8d7c4f4158385868c59154f4a2145efcae68c65d
Andrea Cacioli <andrea.cacioli@edu.unito.it>
2023-03-27 19:23:16 +0200

    Completed the Grammar from the Jurafsky Draft book and renamed the file

---
+++ Parte1-Mazzei/EnglishGrammar.py
@@ -0,0 +1,38 @@
+from nltk import CFG
+
+englishGrammar = CFG.fromstring("""
+
+        S -> NP VP
+        S -> VP
+        S -> X1 VP
+        X1 -> Aux NP
+        S -> book | include | prefer
+        S -> Verb NP
+        S -> X2 PP
+        S -> Verb PP
+        S -> VP PP
+        NP -> Pronoun
+        NP -> Proper-Noun
+        NP -> Det Nominal
+        Nominal -> Noun
+        Nominal -> Nominal Noun
+        Nominal -> Nominal PP
+        VP -> Verb
+        VP -> Verb NP
+        VP -> X2 PP
+        VP -> Verb PP
+        VP -> VP PP
+        X2 -> Verb NP
+        PP -> Preposition NP
+
+        Det -> that | this | the | a
+        Noun -> book | flight | meal | money
+        Verb -> book | include | prefer
+        Pronoun -> I | she | me
+        Proper-Noun -> Huston | NWA
+        Aux -> does
+        Preposition -> from | to | on | near | through
+
+""")
+
+print(englishGrammar.productions())
