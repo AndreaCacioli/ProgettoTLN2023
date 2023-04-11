f61810c2c3239129004d6bc3a7a0c65d70f9783f
Andrea Cacioli <andrea.cacioli.1c@gmail.com>
2023-04-12 01:01:25 +0200

    First bfs implementation: actually kinda slow

---
+++ Parte2-Radicioni/distance.py
@@ -0,0 +1,40 @@
+from nltk.corpus import wordnet
+from Lesk import simplifiedLesk
+
+def bfs(start, goal):
+    addedBy = {}
+    visited = set([])
+    queue = [start]
+    while len(queue) > 0:
+        print(f"length is now {len(queue)}")
+        current = queue.pop(0)
+        if current == goal:
+            return getPath(addedBy, start, goal)
+        visited.add(current)
+        for sense in getNeighbours(current):
+            if sense not in visited:
+                queue.append(sense)
+                addedBy[sense] = current
+
+def getPath(addedByDictionary, start, goal):
+    ret = []
+    element = goal
+    while element != start:
+        ret.append(element)
+        element = addedByDictionary[element]
+    ret.append(start)
+    ret.reverse()
+    return ret
+
+def getNeighbours(sense):
+    neighbours = []
+    neighbours.extend(sense.hyponyms())
+    neighbours.extend(sense.instance_hyponyms())
+    neighbours.extend(sense.hypernyms())
+    neighbours.extend(sense.instance_hypernyms())
+    return neighbours
+
+start = simplifiedLesk("thing", "")
+end = simplifiedLesk("entity", "")
+
+print(bfs(start, end))
