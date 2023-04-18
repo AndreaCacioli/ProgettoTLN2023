934ec825b69a637f1449d3bf0b5bdb9e844ffcbd
Andrea Cacioli <andrea.cacioli@edu.unito.it>
2023-04-18 16:41:31 +0200

    Almost over

--- Parte2-Radicioni/SemcorTest.py
+++ Parte2-Radicioni/SemcorTest.py
@@ -1,5 +1,7 @@
 from nltk.corpus import semcor
 from nltk.corpus import wordnet
+import random
+from Lesk import simplifiedLesk
 
 def getSynset(elem):
     lemma = elem.label()
@@ -36,10 +38,34 @@ def isEndingElement(elem):
     return elem.label() is None and elem.leaves()[0] == '.'
 
 # TODO figure out why 23 crashes
-N = 22
+total_tested_sentences = 0
+i = 0
+sents = semcor.sents()
 
 if __name__ == "__main__":
-    for sentence in semcor.sents()[0:N]:
+    while(total_tested_sentences < 50):
+        sentence = sents[i]
+        i += 1
         elems = getTaggedElements(sentence)
-        print(elems)
-        print()
+        if elems is not None:
+            trovato = False
+            while not trovato:
+                randElement = random.choice(elems)
+                try:
+                    synset = getSynset(randElement)
+                    if synset.pos() == wordnet.NOUN:
+                        trovato = True
+                except:
+                    continue
+            # We have the synset, let's try disambiguating it with close words
+            span = 3 # 3 words left and 3 words right (when possible)
+            termine = randElement.leaves()[0]
+            print(termine)
+            print(sentence)
+            index = sentence.index(termine)
+            print(f"Trovato {termine} index: {index}")
+            context = sentence[max(0, index - span): min(len(sentence)-1, index + span)]
+            disambiguation = simplifiedLesk(termine, context)
+            total_tested_sentences +=1
+            print(f"Succeded: {synset == disambiguation}")
+            
