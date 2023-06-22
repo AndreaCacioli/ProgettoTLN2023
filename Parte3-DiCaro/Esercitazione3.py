bb5a2bddef4b7b01ba8eb58efce078e98fcb813f
Andrea Cacioli <andrea.cacioli@edu.unito.it>
2023-06-22 16:03:40 +0200

    Finita Esercitazione3

--- Parte3-DiCaro/Esercitazione3.py
+++ Parte3-DiCaro/Esercitazione3.py
@@ -3,6 +3,7 @@ from nltk.corpus import wordnet
 import spacy
 from spacy import displacy
 from progress_bar import InitBar
+from nltk.wsd import lesk
 
 
 def get_dictionary():
@@ -40,6 +41,7 @@ def print_token(token):
 
 def get_sentences(strings, target, target_pos):
     ret = []
+    indexes = []
     nlp = spacy.load("en_core_web_sm")
     pbar = InitBar("Sentence Segmentation and filtering")
     for i, s in enumerate(strings):
@@ -51,9 +53,10 @@ def get_sentences(strings, target, target_pos):
                         token, target_pos
                     ):
                         ret.append(sent)
+                        indexes.append(i)
                         break
         pbar(i / len(strings) * 100)
-    return ret
+    return ret, indexes
 
 
 def is_pos(token, pos="VERB"):
@@ -83,22 +86,65 @@ def get_sub_verb_obj(doc, target_verb):
     return (subject_, verb, object_)
 
 
+def is_pronoun(string):
+    return (
+        string in ["I"]
+        or string.lower() in ["you", "he", "she", "it", "we", "you", "they"]
+        or string.lower() in ["your", "his", "hers", "its", "our", "their"]
+        or string.lower() in ["him", "her", "them"]
+    )
+
+
+def get_synset(sentence, word):
+    if is_pronoun(word):
+        return "-PRON-"
+    else:
+        return lesk(sentence, word, "n")
+
+
+def get_type(synset, dictionary):
+    if type(synset) is str and synset == "-PRON-":
+        return "PERSON"
+    else:
+        try:
+            return dictionary[synset]
+        except:
+            return "UNKNOWN"
+
+
 if __name__ == "__main__":
     strings = []
     TARGET = "watch"
     TARGET_POS = "VERB"
     with open(CORPUS_PATH) as file:
         csv_file = csv.reader(file)
-        for line in list(csv_file)[1:5000]:
+        for line in list(csv_file)[1:]:
             strings.append(line[-1])
 
-    sentences = get_sentences(strings, TARGET, TARGET_POS)
+    sentences, indexes = get_sentences(strings, TARGET, TARGET_POS)
     tuples = []
-    for sentence in sentences:
+    for sentence, i in zip(sentences, indexes):
         print(sentence)
         tuple = get_sub_verb_obj(sentence, TARGET)
         print(tuple)
         print()
         if tuple[0] != "" and tuple[2] != "":
-            tuples.append(tuple)
-    print(tuples)
+            tuples.append((tuple, i))
+    print("Loading semantic types...")
+    dictionary = get_dictionary()
+    print("Disambiguation of the tuples...")
+    semantic_types_counts = {}
+    for tuple, i in tuples:
+        sentence = strings[i].split()
+        subject_synset = get_synset(sentence, tuple[0])
+        object_synset = get_synset(sentence, tuple[2])
+        sub_type = get_type(subject_synset, dictionary)
+        obj_type = get_type(object_synset, dictionary)
+        try:
+            semantic_types_counts[(sub_type, obj_type)] += 1
+        except:
+            semantic_types_counts[(sub_type, obj_type)] = 1
+    semantic_types_counts = dict(
+        sorted(semantic_types_counts.items(), key=lambda item: item[1], reverse=True)
+    )
+    print(semantic_types_counts)
