f79ae53eff8236d00559e8c82f27ea10e742b81a
Andrea Cacioli <andrea.cacioli@edu.unito.it>
2023-06-26 12:12:32 +0200

    Started reading the file

--- Parte3-DiCaro/Esercitazione4.py
+++ Parte3-DiCaro/Esercitazione4.py
@@ -1,3 +1,22 @@
 # Text Segmentation
 # We need to write a program that differentiates between different topics written in the same file
+import spacy
 
+def read_file(path):
+    separation = 0
+    sentences = [[]]
+    i = 0
+    with open(path) as file:
+        lines  = file.readlines()
+        for line in lines:
+            if line == '---':
+                separation += 1
+                i+=1
+            else:
+                sentences[i].append(line)
+
+
+PATH = './Parte3-DiCaro/TextSegmentation.txt'
+
+if __name__ == "__main__":
+    read_file(PATH)
