bed2ea674020c1345e391bf55ce2b917c40c0298
Andrea Cacioli <andrea.cacioli@edu.unito.it>
2023-07-03 13:29:27 +0200

    Getting synsets from json file

--- Parte3-DiCaro/Esercitazione6.py
+++ Parte3-DiCaro/Esercitazione6.py
@@ -7,6 +7,7 @@ import json
 import os
 from Esercitazione5 import preprocess
 from nltk.wsd import lesk
+from nltk.corpus import wordnet as wn
 import pandas
 
 
@@ -41,8 +42,14 @@ def get_senses(stories):
     return ret
 
 
-if __name__ == "__main__":
-    print("Collecting Data from the json dataset...")
+def get_basicness_score(synsets):
+    """
+    A simple function that returns a score in the interval [0:1] proportional to the basicness of a synset.
+    A score of 0 means that the sense is *NOT* basic at all, while a score of 1 is a basic word
+
+    :param ``synsets``: a list of WordNet synsets we want a basicness score of.
+    :returns a list of scores relative to each synset of the list
+    """
     PATH = "./Parte3-DiCaro/BasicAdvanced.json"
     BEDTIME_DIR = "./Parte3-DiCaro/Bed Time Stories"
     RESEARCH_ABSTRACTS_PATH = "./Parte3-DiCaro/Abstracts/ResearchAbstracts.csv"
@@ -51,25 +58,29 @@ if __name__ == "__main__":
     synsets_strings = data["dataset"]
     answers = data["answers"]
     time_diffs = data["timeDiffs"]
-    for i in range(5):
-        print(synsets_strings[i])
-        print(answers[i])
-        print(time_diffs[i])
+    synsets = []
+    for synset_string in synsets_strings:
+        s = synset_string.split('(')
+        s = s[1].split(')')[0]
+        s = s[1:len(s) -1]
+        synset = wn.synset(s)
+        print(synset)
+        synsets.append(synset)
 
-    print()
     stories_preprocessed = read_bed_time_stories(BEDTIME_DIR)
     basic_senses = get_senses(stories_preprocessed)
-    for i in range(3):
-        print(stories_preprocessed[i][:20])
-    for i in range(3):
-        print(basic_senses[i][:10])
 
-    print()
     abstracts_strings = pandas.read_csv(RESEARCH_ABSTRACTS_PATH)
     abstracts = abstracts_strings.loc[:, "ABSTRACT"].tolist()
+    abstracts = abstracts[:500]
     abstracts_preprocessed = preprocess(abstracts)
     abstracts_senses = get_senses(abstracts_preprocessed)
-    for i in range(3):
-        print(abstracts_preprocessed[i][:20])
-    for i in range(3):
-        print(abstracts_senses[i][:10])
+
+    return 1
+
+
+if __name__ == "__main__":
+    synsets = []
+    synsets.append(wn.synset("dog.n.01"))
+    scores = get_basicness_score(synsets)
+    print(scores)
